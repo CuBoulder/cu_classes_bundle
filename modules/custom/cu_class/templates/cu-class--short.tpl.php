e491f6a81eb65f5aa3e70b0135f9bdea23120d77
kevincrafts <kevin.crafts@colorado.edu>
2016-01-13 09:53:31 -0700

    class/course styling

--- modules/custom/cu_class/templates/cu-class--short.tpl.php
+++ modules/custom/cu_class/templates/cu-class--short.tpl.php
@@ -1,12 +1,26 @@
+<?php dpm($content); ?>
 <div class="class-view-mode-short class-entity-wrapper">
   <div class="class-wrapper">
     <div class="class-title-wrapper">
-      <h2 class="class-title"><?php print $format; ?> <span>Section <?php print $content['field_class_class_section'][0]['#markup']; ?></span></h2>
+      <h2 class="class-format"><?php print $format; ?></h2>
     </div>
-
     <div class="class-content">
       <div class="row clearfix">
         <div class="class-meta class-section-parent col-lg-6 col-md-6 col-sm-6 col-xs-12">
+          <div class="row clearfix">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+              <div class="class-section">
+                <h3>Section</h3>
+                <?php print $content['field_class_class_section'][0]['#markup']; ?>
+              </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+              <div class="class-section">
+                <h3>Session</h3>
+                <?php print $session; ?>
+              </div>
+            </div>
+          </div>
           <div class="row clearfix">
             <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
               <div class="class-section">
@@ -15,40 +29,33 @@
               </div>
             </div>
             <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-              <div class="class-section">
-                <h3>Core Requirements</h3>
-                <?php print $course_requirements; ?>
-              </div>
+              <?php if (!empty($course_requirements)): ?>
+                <div class="class-section">
+                  <h3>Core Requirements</h3>
+                  <?php print $course_requirements; ?>
+                </div>
+              <?php endif; ?>
             </div>
           </div>
-
-          <div class="row clearfix">
-            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-              <div class="class-section">
-                <h3>Credits</h3>
-                <?php print render($content['field_class_units_acad_prog']); ?>
-              </div>
-            </div>
-            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-              <div class="class-section">
-                <h3>Instructor</h3>
-                <?php print render($content['field_class_ssr_instr_long']); ?>
-              </div>
-            </div>
-          </div>
-
-
         </div>
         <div class="class-date-instructor col-lg-3 col-md-3 col-sm-3 col-xs-12">
           <div class="class-section">
             <h3>Course Dates</h3>
             <?php print $start_date; ?> - <?php print $end_date; ?>
           </div>
-          <div class="class-section">
-            <h3>Session</h3>
-            <?php print $session; ?>
-          </div>
+          <?php if(!empty($content['field_class_ssr_instr_long'][0]['#markup'])): ?>
+            <div class="class-section">
+              <h3>Instructor</h3>
+              <?php print render($content['field_class_ssr_instr_long']); ?>
+            </div>
+          <?php endif; ?>
 
+          <?php if(!empty($content['field_class_ssr_mtg_loc_long'][0]['#markup'])): ?>
+            <div class="class-section">
+              <h3>Location</h3>
+              <?php print render($content['field_class_ssr_mtg_loc_long']); ?>
+            </div>
+          <?php endif; ?>
         </div>
         <div class="class-availabilitycol-lg-3 col-md-3 col-sm-3 col-xs-12">
           <div class="class-section">
--- modules/custom/cu_class/templates/views-view-fields--cu-classes.tpl.php
+++ modules/custom/cu_class/templates/views-view-fields--cu-classes.tpl.php
@@ -1,8 +1,10 @@
 <?php
-
+//dpm($fields);
 // Create class title
 $class_title = array();
-$class_title[] = html_entity_decode($fields['field_class_course_title_long']->content) . ' - Section ' . $fields['field_class_class_section']->content;
+$class_title[] = '<h2 class="class-title">' .  html_entity_decode($fields['field_class_course_title_long']->content) . '</h2>';
+$class_title[] = '<h3 class="class-number">' .  $fields['field_class_subject']->content . ' ' . $fields['field_class_catalog_nbr']->content . '</h3>';
+//$class_title[] =  '<span>Section ' . $fields['field_class_class_section']->content . '</span>';
 
 if (isset($fields['field_class_instruction_mode']->content)) {
   $mode = $fields['field_class_instruction_mode']->content;
@@ -10,7 +12,7 @@ if (isset($fields['field_class_instruction_mode']->content)) {
 }
 
 // Link class title to course page
-$class_title_link = l(join(' ', $class_title), 'course/' . $fields['field_class_crse_id']->content, array('html' => TRUE, 'query' => array('class_id' => $fields['field_class_class_nbr']->content)));
+$class_title_link = l(join(' ', $class_title), 'course/' . $fields['field_class_crse_id']->content, array('html' => TRUE, 'query' => array('class_id' => $fields['field_class_class_nbr']->content), 'attributes' => array('class' => array('class-title-group'))));
 
 // Dates
 $dates = array();
@@ -37,61 +39,30 @@ $session = cu_class_session_translator($fields['field_class_session_code']->cont
 $format = cu_class_format_translate($fields['field_class_instruction_mode']->content);
 ?>
 <div class="class-view-mode-list class-entity-wrapper">
-  <div class="class-wrapper">
-    <div class="class-title-wrapper">
-      <h2 class="class-title"><?php print $class_title_link; ?></h2>
-      <h3 class="class-format"><span class="element-invisible">Class Format: </span><?php print $format; ?></h3>
-    </div>
-
+  <div class="class-row-wrapper">
     <div class="class-content">
       <div class="row clearfix">
-        <div class="class-meta col-lg-6 col-md-6 col-sm-6 col-xs-12">
-          <div class="row clearfix">
-            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-              <div class="class-section">
-                <h3>Class No.</h3>
-                <?php print $fields['field_class_class_nbr']->content; ?>
-              </div>
-            </div>
-            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-              <div class="class-section">
-                <h3>Core Requirements</h3>
-                <?php print $course_requirements; ?>
-              </div>
-            </div>
-          </div>
-          <div class="row clearfix">
-            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-              <div class="class-section">
-                <h3>Credits</h3>
-                <?php print $fields['field_class_units_acad_prog']->content; ?>
-              </div>
-            </div>
-            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-              <div class="class-section">
-                <h3>Instructor</h3>
-                <?php print $fields['field_class_ssr_instr_long']->content; ?>
-              </div>
-            </div>
-          </div>
+        <div class="class-meta col-lg-4 col-md-4 col-sm-4 col-xs-12">
+          <?php print $class_title_link; ?>
         </div>
         <div class="class-date-instructor col-lg-3 col-md-3 col-sm-3 col-xs-12">
           <div class="class-section">
-            <h3>Course Dates</h3>
-            <?php print join(' - ', $dates); ?>
-          </div>
-          <div class="class-section">
-            <h3>Session</h3>
+            <h3 >Session</h3>
             <?php print $session; ?>
           </div>
+
         </div>
-        <div class="class-availability col-lg-3 col-md-3 col-sm-3 col-xs-12">
+        <div class="class-availability col-lg-2 col-md-2 col-sm-2 col-xs-12">
           <div class="class-section">
-            <h3>Availability</h3>
-            <?php print $available_seats; ?>
-            <div class="updated">
-              <?php print $updated; ?>
-            </div>
+            <h3 >Credits</h3>
+            <?php print $fields['field_class_units_acad_prog']->content; ?>
+          </div>
+        </div>
+
+        <div class="class-format col-lg-3 col-md-3 col-sm-3 col-xs-12">
+          <div class="class-section">
+            <h3 >Format</h3>
+            <?php print $format; ?>
           </div>
         </div>
       </div>
